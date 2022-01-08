fd2a4380be0c4b14c8b26b23df150c4f9bc9b71c
Steve Karuma <stevekaruma@gmail.com>
2022-01-08 21:20:32 +0300

    Added api configuration

---
+++ src/app/config/api/index.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const getHeaders = () => {
+  var token = localStorage.getItem("auth_token");
+  if (token) {
+    var headers = {
+      Authorization: `Token ${token}`,
+    };
+  } else {
+    headers = {};
+  }
+  return headers;
+};
+
+const getInstance = ({ useHeader = true }) => {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  return axios.create({
+    baseURL: apiUrl,
+    headers: useHeader ? getHeaders() : null,
+  });
+};
+
+const put = async ({ url, data }) => {
+  return await getInstance().put(url, data);
+};
+
+const patch = async ({ url, data }) => {
+  return await getInstance().patch(url, data);
+};
+
+const remove = async ({ url, data }) => {
+  return await getInstance().delete(url, data);
+};
+
+const get = async ({ url, params }) => {
+  return await getInstance({}).get(url, params);
+};
+
+const post = async ({ url, data, useHeader }) => {
+  return await getInstance({ useHeader: useHeader }).post(url, data);
+};
+
+const Api = {
+  get: get,
+  post: post,
+  put: put,
+  patch: patch,
+  remove: remove,
+};
+
+export default Api;
