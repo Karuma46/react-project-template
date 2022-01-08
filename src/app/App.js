62977d29b7ff97e125115136b84c3f6d3466bff4
Steve Karuma <stevekaruma@gmail.com>
2022-01-08 17:22:27 +0300

    Added Routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,16 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Docs from "./pages/docs";
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route index exact path="/" element={<Docs />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/app/pages/docs/index.js
@@ -0,0 +1,11 @@
+const Docs = () => {
+  return (
+    <>
+      <div>
+        <h1>React Project Template</h1>
+      </div>
+    </>
+  );
+};
+
+export default Docs;
