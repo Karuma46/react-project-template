d3231884d6ce60f55c31ac89706ac2624871bdcb
Steve Karuma <stevekaruma@gmail.com>
2022-03-02 22:12:58 +0300

    Docs Page

--- src/app/pages/docs/index.js
+++ src/app/pages/docs/index.js
@@ -2,7 +2,24 @@ const Docs = () => {
   return (
     <>
       <div>
-        <h1>React Project Template</h1>
+        <h1>Header 1</h1>
+        <h2>Header 2</h2>
+        <h3>Header 3</h3>
+        <h4>Header 4</h4>
+
+        <a href="/">Link</a>
+
+        <p>
+          The quick brown fox jumps over the lazy dog. DJs flock by when MTV ax
+          quiz prog. Jnk MTV quiz graced by foz whelps. Bawds jog, flick quartz,
+          vex nymphs. Waltz, bad nymph
+        </p>
+
+        <ul>
+          <li>List Item 1</li>
+          <li>List Item 2</li>
+          <li>List Item 3</li>
+        </ul>
       </div>
     </>
   );
