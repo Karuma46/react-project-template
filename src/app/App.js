97a7b55d2495d67a21be5e40dbddcb1b7d167556
Steve Karuma <stevekaruma@gmail.com>
2022-03-13 20:24:36 +0300

    Added auth & cart context providers to app

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthContext } from "./pages/auth/context";
 import Docs from "./pages/docs";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index exact path="/" element={<Docs />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthContext>
+        <BrowserRouter>
+          <Routes>
+            <Route index exact path="/" element={<Docs />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext>
     </div>
   );
 };
